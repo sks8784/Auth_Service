0c46bb57249f99ac6c05c9cc2d4f9e2cd567aa70
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-08-10 13:34:29 +0530

    added auth service

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const {PORT}=require('./config/serverConfig');
+const apiRoutes=require('./routes/index');
+
+const db=require('./models/index');
+// const {User,Role}=require('./models/index');
+
+const app=express();
+
+const prepareAndStartServer=()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',apiRoutes);
+    
+    app.listen(PORT,async ()=>{
+        console.log(`Server started on Port: ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+
+        // const u1=await User.findByPk(3);
+        // const r1=await Role.findByPk(2);
+        // const response=await r1.getUsers();
+        // const response=await u1.hasRole(r1);
+        // console.log(response);
+        // u1.addRole(r1);
+    });
+}
+
+prepareAndStartServer();
